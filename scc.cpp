1928cfa063ea7879b0bc787b74c686b1b6f39b4b
Karthik Nishanth <nishanthkarthik@live.com>
2017-12-27 13:03:09 +0530

    add cmd line args to scc

--- scc.cpp
+++ scc.cpp
@@ -1,24 +1,44 @@
 #include "ctranspiler.h"
 #include "iremitter.h"
 #include "parser.h"
+#include <fstream>
 #include <iostream>
 
 using namespace std;
 
-int main()
+int main(int argc, char const* argv[])
 {
-    Parser p(&cin);
-    auto ins = p.parse();
 
-    // print parsed code
-    // for (auto i : ins)
-    // cout << i << endl;
+    if (argc < 3) {
+        cout << "scc llvm|c file|-" << endl;
+        cout << "* llvm ir or c output" << endl;
+        cout << "* file or stdin" << endl;
+        return 1;
+    }
 
-    // convert to c
-    // Transpiler t(&cout, ins);
-    // t.transpile_c();
+    bool isllvm = (string(argv[1]) == "llvm");
+    bool isstdin = (string(argv[2]) == "-");
 
-    // convert to llvm ir
-    IREmitter e(&cout, ins);
-    e.ir_emit();
+    vector<Instruction> ins;
+
+    if (isstdin) {
+        Parser p(&cin);
+        ins = p.parse();
+    } else {
+        ifstream fin(argv[2], ios::in);
+        Parser p(&fin);
+        ins = p.parse();
+        if (fin)
+            fin.close();
+    }
+
+    if (isllvm) {
+        // convert to llvm ir
+        IREmitter e(&cout, ins);
+        e.ir_emit();
+    } else {
+        // convert to c
+        Transpiler t(&cout, ins);
+        t.transpile_c();
+    }
 }
